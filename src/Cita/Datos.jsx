e1568890921b61ab26b7c4f69c29e6606dfdd563
Martin Ku <aaronmartinku09@icloud.com>
2026-04-13 15:40:14 -0300

    Terminar (made by MH)

--- src/Cita/Datos.jsx
+++ src/Cita/Datos.jsx
@@ -3,7 +3,7 @@ function Datos({cita}) {
   return (
     <>
       <p>Mascota: <span>{cita.mascota}</span></p>
-      <p>Dueño: <span>{cita.dueño}</span></p>
+      <p>Dueño: <span>{cita.propietario}</span></p>
       <p>Fecha: <span>{cita.fecha}</span></p>
       <p>Hora: <span>{cita.hora}</span></p>
       <p>Síntomas: <span>{cita.sintomas}</span></p>
--- src/Formulario/Formulario.jsx
+++ src/Formulario/Formulario.jsx
@@ -2,44 +2,39 @@ import Entrada from '../Entrada/Entrada';
 import React, { useState } from "react";
 import './Formulario.css';
 
-function Formulario() {
+function Formulario({agregarCita}) {
   const [cita, setCita] = useState({mascota: "", propietario: "", fecha: "", hora: "", sintomas: ""});
 
-  const handleChange = (e) => {
+  const guardarCambio = (e) => {
     setCita({
       ...cita,
       [e.target.name]: e.target.value
     });
   };
 
-  const handleSubmit = (e) => {
+  const enviarForm = (e) => {
     e.preventDefault();
 
-    agregarCita({
-      ...cita,
-      id: Date.now()
-    });
+    const nuevaCita = {
+      id: Date.now(),
+      ...cita
+    };
 
-    setCita({
-      mascota: "",
-      propietario: "",
-      fecha: "",
-      hora: "",
-      sintomas: ""
-    });
+    console.log(nuevaCita);
+    agregarCita(nuevaCita);
   };
 
   return (
     <div> {}
       <h2>Crear mi Cita</h2>
-      <form onSubmit={handleSubmit}>
-        <Entrada label="Nombre Mascota" type="text" name="mascota" placeholder="Nombre Mascota" />
-        <Entrada label="Nombre Dueño" type="text" name="propietario" placeholder="Nombre dueño" />
-        <Entrada label="Fecha" type="date" name="fecha" />
-        <Entrada label="Hora" type="time" name="hora" />
+      <form onSubmit={enviarForm}> 
+        <Entrada label="Nombre Mascota" type="text" name="mascota" placeholder="Nombre Mascota" value={cita.mascota} onChange={guardarCambio}/>
+        <Entrada label="Nombre Dueño" type="text" name="propietario" placeholder="Nombre dueño" value={cita.propietario} onChange={guardarCambio}/>
+        <Entrada label="Fecha" type="date" name="fecha" value={cita.fecha} onChange={guardarCambio}/>
+        <Entrada label="Hora" type="time" name="hora" value={cita.hora} onChange={guardarCambio}/>
 
         <label>Sintomas</label>
-        <textarea name="sintomas" className="u-full-width"></textarea>
+        <textarea name="sintomas" className="u-full-width" value={cita.sintomas} onChange={guardarCambio}></textarea>
 
         <button type="submit" className="u-full-width button-primary">
           Agregar Cita
